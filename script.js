df10621ae1f0c1011828f1d474267e6bd4d4144e
PolatDeu <ahmetpolat@mein.gmx>
2021-06-13 00:31:13 +0200

    js baslangici

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('Hello from Deutschland')
+console.error('yapmma böle hatalar')
+
+var i=0;
+var sum=0;
+while (i<6){
+    var sum=sum + i;
+    i++;
+}
+console.log("The Total is: " + sum);
